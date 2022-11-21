187966d01bfee5427cd42b88778be7f59323f2f7
Jacques-James <101092451+Jacques-James@users.noreply.github.com>
2022-11-21 04:42:46 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "b" {
-  #bucket = "my-tf-test-bucket"
+# resource "aws_s3_bucket" "b" {
+#   #bucket = "my-tf-test-bucket"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
